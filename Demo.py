8326416e0bada0d270214e09bb53a96fd1f34967
Sayali <119116326+Sayali-H-Thorat@users.noreply.github.com>
2024-02-01 12:32:25 +0530

    Add files via upload

---
+++ Demo.py
@@ -0,0 +1,21 @@
+print("Hello")
+print("Python is easy")
+print("Let's learn Python")
+
+a = 10;
+print("Value of a is:",a)
+b = 10//2  #floor division/Integer division it will print the complete integer value
+print(b)
+c = 10/2  #It will print the value in decimal or float type
+print(c)
+d = 2**5  #here 2 is exponent and 5 is power means how much time you have to multiply 2
+print(d)
+e = 8+2*3  #it use bodmas rule
+print(e)
+f = (8+2)*3   #it first solve the brackets
+print(f)
+g = print('John\'s "book"') # here \ is escape character to print the output properly
+h = print('c:\docs\naina\newyork')  #here \n is considered as new line
+i = print(r'c:\docs\naina\newyork') # here we can use R/r for raw string so that it will print the whole path
+j = print('c:\docs\tiger') #Here t is conisedered as tab for space
+k= print(r'c:\docs\tiger') #here to print string properly we have to use raw string
