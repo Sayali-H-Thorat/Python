1452354ece45d7c22adb89fb5424abd2f42887ef
Sayali <119116326+Sayali-H-Thorat@users.noreply.github.com>
2024-02-03 10:58:43 +0530

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,113 @@
+#Python supports wide range of datatypes -int,float,complex,boolean etc
+
+#None type
+x = None
+print(x)
+print(type(x))
+
+#Numeric types- int
+a = 10
+print(a)
+print(type(a))
+
+#Float datatype
+b = -10.5
+print(b)
+print(type(b))
+
+#complex numbers
+#Here 2 is real part ,3j is imaginary part and j is standard coefficient
+c = 2+3
+print(c)
+print(type(c))
+
+#booleans-True or false
+print(3>4)
+print(1<2)
+
+d = 10>50
+print(d)
+print(type(d))
+
+e = True
+print(e)
+print(type(e))
+
+#Represent numbers in differrent formats- Binary,octal,hexademical
+
+a = 10
+print(bin(a))
+print(hex(a))
+print(oct(a))
+
+#Type conversion/Type casting
+#converting one datatype into another
+
+#1.Convert float into integer
+a = 10.7
+print(a)
+print(type(a))
+
+b = int(a)
+print(b)
+print(type(b))
+
+#2.Convert int to float
+x =20
+print(x)
+print(type(x))
+
+y =float(x)
+print(y)
+print(type(y))
+
+#3.Convert string to integer
+z = '100'
+print(z)
+print(type(z))
+
+a = int(z)
+print(a)
+print(type(a))
+
+#4.Convert int to complex
+
+a = 10
+print(a)
+print(type(a))
+
+b = complex(a)
+print(b)
+print(type(b))
+
+#
+# #5.Complex to integer
+# l = 10+5j
+# print(l)
+# print(type(l))
+#
+# m = int(l)
+# print(m)
+# print(type(m))
+
+#Mutable objects(changeable) and Immutable objects(Not changeable)
+
+#Mutable Objects: These are types that can bechanged-> Sets and Dictionary
+#Immutable objects: These are datatypes that cannot be changed-> String ,tuples,int,float,boolean
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
