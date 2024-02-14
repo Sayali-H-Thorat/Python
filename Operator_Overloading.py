f48f3f75b6625f7a18aa9c1d5fe608e3f08045ce
Sayali <119116326+Sayali-H-Thorat@users.noreply.github.com>
2024-02-14 10:24:03 +0530

    Add files via upload

---
+++ Operator_Overloading.py
@@ -0,0 +1,20 @@
+# operator overloading
+# '+' operator in python is overloaded to perform multiple things
+
+
+
+
+# '+' will add two numbers
+x =10
+y=11
+print("Addition is:",x+y)
+
+# '+' will concatenate two strings
+s1 ='Hello'
+s2='world'
+print("Concatenate two strings:",s1+' '+s2)
+
+# '+' will add two lists
+l1=[1,2,3]
+l2=[5,6,7]
+print("Add two list elements:",l1+l2)
